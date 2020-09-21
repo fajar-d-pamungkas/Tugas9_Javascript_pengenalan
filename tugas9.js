d8c31fe40b281e20061c0ed8cd7852e18011ac4b
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-09-21 09:27:41 +0700

    Add files via upload

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+var dataSiswa = {
+	nama: "Fajar",
+	usia: 20,
+	jk: "Laki-laki",
+	nilai: 75
+}
+// tampilan dataSiswa
+console.log(dataSiswa)
+
+//tampilan dataSiswa menggunakan perulangan for in
+for(var i in dataSiswa){
+	console.log(dataSiswa[i])
+}
